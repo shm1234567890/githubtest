f0c7027e1accc8e6c9d6b4c69c6ae74358c479dc
shm1234567890 <1741610956@qq.com>
2019-09-04 03:08:42 -0400

    测试

--- src/main/java/GitHubText.java
+++ src/main/java/GitHubText.java
@@ -1,5 +1,7 @@
 public class GitHubText {
     public static void main(String[] args) {
         System.out.println("gitTest");
+        System.out.println("push");
+        System.out.println("测试");
     }
 }
